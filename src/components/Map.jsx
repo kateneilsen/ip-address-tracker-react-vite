3d5ed20901bbbbf94d47d79e02d2d13d12aad4f8
Kate <neilsenk09@gmail.com>
2023-10-02 15:45:33 -0400

    onenter search

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 import { MapContainer, TileLayer, Marker } from "react-leaflet";
-import L from "leaflet";
 import "leaflet/dist/leaflet.css";
-import icon from "leaflet/dist/images/marker-icon.png";
-import iconShadow from "leaflet/dist/images/marker-shadow.png";
 
 const Map = ({ ipInfo }) => {
   return (
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import styles from "./styles.module.css";
 
 const Search = ({ query, setQuery, handleSearch, error }) => {
+  const handleOnEnter = (e) => {
+    if (e.key === "Enter") {
+      handleSearch();
+    }
+  };
   return (
     <section className={styles.top}>
       <h1>IP Address Tracker</h1>
@@ -11,7 +16,10 @@ const Search = ({ query, setQuery, handleSearch, error }) => {
           onChange={(e) => setQuery(e.target.value)}
           placeholder="Search for any IP address or domain"
         />
-        <button onClick={() => handleSearch()}>
+        <button
+          onClick={() => handleSearch()}
+          onKeyDown={(e) => handleOnEnter(e)}
+        >
           <img src="/icon-arrow.svg" alt="arrow right" />
         </button>
         {error !== "" ? <p className={styles.error}>{error}</p> : ""}
